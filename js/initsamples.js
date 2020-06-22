2b85d3a6759fe9c295d7a30ec4ecc8a3cf4a6e8d
Krishnabh Medhi <krishnabhm@qualtrics.com>
2020-06-22 13:30:01 -0700

    Removed all ads stuff

--- js/initsamples.js
+++ js/initsamples.js
@@ -37,13 +37,7 @@ if (window.innerWidth > 786) {
 
 var createSamples = function() {
     for (var i = 0; i < numSamplesToShow; i++) {
-        var imgContDiv;
-        //if (samplesIndex % 5 == 3) {
-        if (samplesIndex % 10 == 6 || samplesIndex % 10 == 9) {
-            imgContDiv = sampler.getAdContainer2();
-        } else {
-            imgContDiv = sampler.getImageContainer(imgUrlFormat + randoms[samplesIndex] + ".jpg");
-        }
+        var imgContDiv = sampler.getImageContainer(imgUrlFormat + randoms[samplesIndex] + ".jpg");
         cols[samplesIndex % numCols].append(imgContDiv);
         samplesIndex++;
         if (samplesIndex >= randoms.length) {
@@ -56,45 +50,6 @@ var createSamples = function() {
     for (var i=0; i<cols.length; i++) {
         inspImages.append(cols[i]);
     }
-}
-
-var getAdContainer2 = function(src) {
-    var adCont = document.createElement("div");
-    adCont.className = "img-cont-generic suggest-img-div ad-cont";
-    var adTitle = document.createElement("span");
-    adTitle.className = "ad-title";
-    adTitle.innerHTML = "Advertisement";
-    adCont.append(adTitle);
-    
-    var ad_id = "block_" + adsIndex;
-    var ad = document.createElement("div");
-    ad.id = ad_id;
-    sampler.adUnits.push({
-        code: ad_id,
-        placement_id: adsIndex,
-        sizes: [300, 250]
-    });
-    smarty.buildUnits(sampler.adUnits);
-
-    adCont.append(ad);
-    adsIndex++;
-    return adCont;
-}
-
-var getAdContainer = function(src) {
-    var adCont = document.createElement("div");
-    adCont.className = "img-cont-generic suggest-img-div ad-cont";
-    var adTitle = document.createElement("span");
-    adTitle.className = "ad-title";
-    adTitle.innerHTML = "Advertisement";
-    adCont.append(adTitle);
-    var ad = document.createElement("script");
-    ad.setAttribute("data-cfasync", "false");
-    ad.setAttribute("type", "text/javascript");
-    ad.src = "//p403126.clksite.com/adServe/banners?tid=403126_791173_" + adsIndex;
-    adCont.append(ad);
-    adsIndex++;
-    return adCont;
 }
 
 var getImageContainer = function(src) {
@@ -159,12 +114,9 @@ function resizeImageToSpecificWidth(img, width) {
 
 var sampler = {
     createSamples: createSamples,
-    getImageContainer: getImageContainer,
-    getAdContainer: getAdContainer,
-    getAdContainer2: getAdContainer2,
+    getImageContainer: getImageContainer
     renderPreviewImage: renderPreviewImage,
-    resizeImageToSpecificWidth: resizeImageToSpecificWidth,
-    adUnits: []
+    resizeImageToSpecificWidth: resizeImageToSpecificWidth
 };
 
 sampler.createSamples();
